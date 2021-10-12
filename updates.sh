1a1633f1024a718a6c38bcddbbb6be95c0f1f429
patricia_xiao <PatriciaXiao@users.noreply.github.com>
2021-10-12 00:31:09 -0700

    Update updates.sh

--- updates.sh
+++ updates.sh
@@ -1,5 +1,5 @@
 git pull --rebase
-chmod a+r files/Fall2021_ReadingGroup_AlgorithmicGNN.pdf
+chmod a+r files/ReadingGroup_DGM_2021Win.pdf
 chmod a+r index.html
 git add .
 git commit -m "remote updates"
